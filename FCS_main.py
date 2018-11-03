49ecc67def1d1d6627208d52425e6242a86aee33
PackingPeanuts <PackingPeanuts@users.noreply.github.com>
2018-11-02 22:31:26 -0700

    GUI formatting
    Set up layout of program window
    
    Addd about menu
    
    set up entry acquisition

--- FCS_main.py
+++ FCS_main.py
@@ -1,43 +1,91 @@
 from tkinter import *
+from tkinter import messagebox
 
 
-def Generator():
-    print("You are a loser")
+# define main variables for performing FCS procedures
+
+
+# Define Functions for GUI Functionality
+def store_data(event):
+    if data_entry.get():
+        print(data_entry.get())
     return
 
 
-def verifier():
-    print("You are a winner")
+def store_generator(event):
+    if generator_entry.get():
+        print(generator_entry.get())
     return
 
 
-def toPoly():
-    print("You are not nice")
+def store_received(event):
+    if received_entry.get():
+        print(received_entry.get())
     return
 
 
-# create and format window
-FCSProg = Tk()
+def store_error(event):
+    if error_entry.get():
+        print(error_entry.get())
+    return
 
-FCSProg.winfo_toplevel().title("FCS Project")
-FCSProg.winfo_toplevel().geometry("700x500")
 
-topFrame = Frame(FCSProg, width=650, height=100)
-topFrame.pack()
-bottomFrame = Frame(FCSProg, width=650, height=350)
-bottomFrame.pack(side=TOP)
+def about():
+    messagebox.showinfo("Program Authors",
+                        "Adrian Martinez\nSimerjit Nagra\nHamed Seyedroudbari")
+    return
 
-titleLabel = Label(topFrame, text="ECE 562 FCS Project", fg="blue")
 
-titleLabel.pack(fill=X)
 
-button1 = Button(bottomFrame, text="Enter Generator Sequence", fg="blue", command=Generator)
-button2 = Button(bottomFrame, text="Enter Received Sequence", fg="blue", command=verifier)
-button3 = Button(bottomFrame, text="Enter Error Sequence", fg="blue", command=toPoly)
 
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
+root = Tk()
 
-# run main loop
-FCSProg.mainloop()
+# Format Window Appearance
+root.winfo_toplevel().title("FCS Project")
+root.winfo_toplevel().geometry("600x500")
+root.configure(bg="gray20")
+root.resizable(0, 0)
+
+# Add Menu Items
+about_menu = Menu()
+root.configure(menu=about_menu)
+about_menu.add_command(label="About", command=about)
+
+# Add Main Title
+title_label = Label(text="ECE 562 FCS Project", fg="white", font="default 20 bold", bg="gray30")
+title_label.grid(row=0, column=4, columnspan=5, sticky=W+E+N+S, pady=20)
+
+# Entry Widgets For Entering Sequence Data
+data_entry = Entry(width=40)
+generator_entry = Entry(width=40)
+received_entry = Entry(width=40)
+error_entry = Entry(width=40)
+
+data_entry.bind("<Return>", store_data)
+generator_entry.bind("<Return>", store_generator)
+received_entry.bind("<Return>", store_received)
+error_entry.bind("<Return>", store_error)
+
+# Buttons For Entering Sequence Data
+button1 = Button(text="Enter data bits", bg="steel blue")
+button2 = Button(text="Enter Generator sequence", bg="medium purple")
+button3 = Button(text="Enter Received Sequence", bg="medium sea green")
+button4 = Button(text="Enter Error Sequence", bg="pale violet red")
+
+button1.bind("<Button-1>", store_data)
+button2.bind("<Button-1>", store_generator)
+button3.bind("<Button-1>", store_received)
+button4.bind("<Button-1>", store_error)
+
+button1.grid(row=3, sticky=E, pady=5, padx=2)
+button2.grid(row=4, sticky=E, pady=5, padx=2)
+button3.grid(row=5, sticky=E, pady=5, padx=2)
+button4.grid(row=6, sticky=E, pady=5, padx=2)
+
+data_entry.grid(row=3, column=3, columnspan=2)
+generator_entry.grid(row=4, column=3, columnspan=2)
+received_entry.grid(row=5, column=3, columnspan=2)
+error_entry.grid(row=6, column=3, columnspan=2)
+
+# Run Main Loop
+root.mainloop()
