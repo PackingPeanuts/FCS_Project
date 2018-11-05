2a531148f1f9c3e2cda8c15c157e44283f04b010
PackingPeanuts <PackingPeanuts@users.noreply.github.com>
2018-11-04 20:31:18 -0800

    Fixed things
    Fixed xor and modulo_division
    
    Integrated Alter function in GUI
    
    need to add a button to clear all entries

--- FCS_prog.py
+++ FCS_prog.py
@@ -8,14 +8,16 @@ def check_ones_zeros(strg):
 
 # Function for performing xor across two strings of 0's and 1's
 def xor(a, b):
-    result = []
 
-    for i in range(1, len(b)):
-        if a[i] == b[i]:
-            result.append('0')
-        else:
-            result.append('1')
-    return ''.join(result)
+    if len(a) == len(b):
+        result = []
+
+        for i in range(0, len(a)):
+            if a[i] == b[i]:
+                result.append('0')
+            else:
+                result.append('1')
+        return ''.join(result)
 
 
 # Function for performing Mod2 Division
@@ -28,21 +30,24 @@ def modulo_div(divisor, dividend):
     # work through the division
     while extract < len(dividend):
         if portion[0] == '1':
-            portion = xor(divisor, portion) + dividend[extract]
+            compute = xor(divisor, portion)
+            portion = compute + dividend[extract]
         else:
-            portion = xor('0'*extract, portion) + dividend[extract]
+            compute = xor('0'*len(divisor), portion)
+            portion = compute + dividend[extract]
+        portion = portion[1:]
         extract += 1
 
     # compute remainder
     if portion[0] == '1':
         remainder = xor(divisor, portion)
     else:
-        remainder = xor('0'*extract, portion)
+        remainder = xor('0'*len(divisor), portion)
 
-    return remainder
+    return remainder[1:]
 
 
-def generator(msg, gen_seq, tx_seq):
+def generator(msg, gen_seq, tx_seq, term):
 
     if msg and gen_seq:
         # Calculate padding length
@@ -51,51 +56,108 @@ def generator(msg, gen_seq, tx_seq):
         # Pad Message bits with Zeros before converting to binary
         padding = '0' * n
         messagep = msg + padding
+        fcs_bits = modulo_div(gen_seq, messagep)
 
-        transmitted = msg + modulo_div(gen_seq, messagep)
+        transmitted = msg + fcs_bits
         tx_seq.set(transmitted)
+        term.configure(state="normal")
+        term.insert('end', "\nFrom %s and %s, the FCS bits %s were generated\n" % (msg, gen_seq, fcs_bits))
+        term.insert('end', "\nThe transmitted sequence is %s\n" % transmitted)
+        term.see("end")
+        term.configure(state="disabled")
     else:
         tk.messagebox.showwarning("Missing Parameter", "Please specify both a\nmessage and generator\nsequence.")
     return
 
 
-def verifier(tx_seq, gen_seq):
+def verifier(tx_seq, gen_seq, term):
 
     if tx_seq:
-        # Calculate padding length
-        n = len(gen_seq) - 1
 
-        success = (set(modulo_div(gen_seq, tx_seq)) == set('0'))
+        success = (set(modulo_div(gen_seq, tx_seq)) <= set('0'))
 
-        error_noerror(success)
         if success:
-            print("The FCS bits were generated correctly")
+            term.configure(state="normal", fg="green")
+            term.insert('end', "\nNo Error detected in received sequence\n")
+            term.see("end")
+            term.configure(state="disabled")
         else:
-            print("The FCS bits were generated incorrectly")
+            term.configure(state="normal", fg="red")
+            term.insert('end', "Error detected in received sequence\n")
+            term.see("end")
+            term.configure(state="disabled")
 
     else:
         tk.messagebox.showwarning("Missing Tx", "Please generate a transmitted sequence first.")
     return
 
 
-def receiver(size_of_tx, rx_seq, gen_seq):
+def receiver(size_of_tx, rx_seq, gen_seq, term):
     if rx_seq and size_of_tx:
         if size_of_tx != len(rx_seq):
-            print("Error Occurred\n Incorrect number of bits were received")
+            term.configure(state="normal")
+            term.insert('end', "\nError Occurred\nIncorrect number of bits were received\n")
+            term.see("end")
+            term.configure(state="disabled")
         else:
             check = (set(modulo_div(gen_seq, rx_seq)) == set('0'))
-            error_noerror(check)
+            if check:
+                term.configure(state="normal")
+                term.insert('end', "\nI was unable to detect error in the received Frame\n")
+                term.see("end")
+                term.configure(state="disabled")
+            else:
+                term.configure(state="normal")
+                term.insert('end', "\nI have detected error in this frame\n")
+                term.see("end")
+                term.configure(state="disabled")
     else:
         tk.messagebox.showwarning("Missing Rx", "Please Specify a received sequence first.")
 
     return
 
 
-def error_noerror(bool):
-    if bool:
-        print("I detected no error in the received Frame")
+def alter(gen, trans, error_seq, term):
+
+    if not trans:
+        tk.messagebox.showwarning("Missing Tx", "Please generate a transmitted sequence first.")
+    elif not error_seq:
+        tk.messagebox.showwarning("Missing Ex", "Enter your Error Sequence!")
     else:
-        print("There is no doubt in my mind, you didn't get it")
+        error_length = len(error_seq)
+        trans_length = len(trans)
+        gen_length = len(gen)
+        flag = 0
+        while flag == 0:
+            if trans_length == error_length:
+                flag = 1
+            else:
+                tk.messagebox.showwarning("Length Error!", "Enter an Error Sequence with length %d" % trans_length)
+                return
+
+        received_sequence = xor(trans, error_seq)
+        term.configure(state="normal")
+        term.insert('end', "\nThis error sequence produces the received sequence %s\n" % received_sequence)
+        term.see("end")
+        term.configure(state="disabled")
+
+        remainder = modulo_div(gen, received_sequence)
+
+        term.configure(state="normal")
+        term.insert('end', "\nThe remainder obtained is %s\n" % remainder)
+        term.see("end")
+        term.configure(state="disabled")
+
+        if remainder == '0'*(gen_length-1):
+            term.configure(state="normal", fg="green")
+            term.insert('end', "\nNo error detected in received sequence\n")
+            term.see("end")
+            term.configure(state="disabled")
+        else:
+            term.configure(state="normal", fg="red")
+            term.insert('end', "\nError detected in received sequence\n")
+            term.see("end")
+            term.configure(state="disabled")
     return
 
 
@@ -125,6 +187,7 @@ def store_entry(destination, entry, remove_lead_zeros):
 
 # Define Fonts to use throughout frames
 LARGE_FONT = ("Default", 20, "bold")
+OUTPUT_FONT = ("Default", 10)
 
 
 # Define GUI and Frames
@@ -173,12 +236,19 @@ class MainPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg="gray20")
         self.pack(fill="both", expand="true")
-        self.grid_rowconfigure(1, minsize=15)
-        self.grid_rowconfigure(4, minsize=15)
-        self.grid_rowconfigure(6, minsize=15)
+        self.grid_rowconfigure(1, minsize=10)
+        self.grid_rowconfigure(4, minsize=10)
+        self.grid_rowconfigure(6, minsize=10)
         title_label = tk.Label(self, text="ECE 562 FCS Project", fg="white", bg="gray30",
                                font=LARGE_FONT, relief="groove", bd=2, justify="center")
         title_label.grid(row=0, column=0, columnspan=10, pady=20, padx=225)
+
+        output_terminal = tk.Text(self, fg="white", bg="black", font=OUTPUT_FONT,
+                                  bd=2, height=8, width=55, wrap="word")
+        output_terminal.grid(row=11, column=0, columnspan=10, rowspan=4, pady=10, padx=20)
+        output_terminal.insert('end', "Hello Professor Zahid. Welcome to FCS project!\n")
+        output_terminal.see("end")
+        output_terminal.config(state="disabled")
 
         width_entry_slot = 30
         width_entry_buttons = 22
@@ -195,14 +265,17 @@ class MainPage(tk.Frame):
         message_entry = tk.Entry(self, width=width_entry_slot)
         gensequ_entry = tk.Entry(self, width=width_entry_slot)
         receive_entry = tk.Entry(self, width=width_entry_slot)
+        alterer_entry = tk.Entry(self, width=width_entry_slot)
 
         message_entry.bind("<Return>", lambda x: store_entry(msg_seq, message_entry, 0))
         gensequ_entry.bind("<Return>", lambda x: store_entry(gsq_seq, gensequ_entry, 1))
         receive_entry.bind("<Return>", lambda x: store_entry(rx_seq, receive_entry, 0))
+        alterer_entry.bind("<Return>", lambda x: store_entry(ex_seq, alterer_entry, 0))
 
         message_entry.grid(row=2, column=3, columnspan=2)
         gensequ_entry.grid(row=3, column=3, columnspan=2)
         receive_entry.grid(row=8, column=3, columnspan=2)
+        alterer_entry.grid(row=9, column=3, columnspan=2)
 
         # Define Storage Buttons for Obtaining bit Sequences
         message_button = tk.Button(self, text="Store Message Bits",
@@ -217,10 +290,15 @@ class MainPage(tk.Frame):
                                    width=width_entry_buttons,
                                    bg="medium purple")
         receive_button.bind("<ButtonRelease-1>", lambda x: store_entry(rx_seq, receive_entry, 0))
+        alterex_button = tk.Button(self, text="Store Alter Sequence",
+                                   width=width_entry_buttons,
+                                   bg="medium purple")
+        alterex_button.bind("<ButtonRelease-1>", lambda x: store_entry(ex_seq, alterer_entry, 0))
 
         message_button.grid(row=2, column=1, pady=pady_entry_buttons, padx=padx_entry_buttons)
         gensequ_button.grid(row=3, column=1, pady=pady_entry_buttons, padx=padx_entry_buttons)
         receive_button.grid(row=8, column=1, pady=pady_entry_buttons, padx=padx_entry_buttons)
+        alterex_button.grid(row=9, column=1, pady=pady_entry_buttons, padx=padx_entry_buttons)
 
         # Define Command Buttons to Execute Programs
         generat_button = tk.Button(self, text="Generate Tx",
@@ -236,9 +314,14 @@ class MainPage(tk.Frame):
                                    width=width_command_buttons,
                                    bg="indian red2")
 
-        generat_button.bind("<ButtonRelease-1>", lambda x: generator(msg_seq.get(), gsq_seq.get(), tx_seq))
-        verifie_button.bind("<ButtonRelease-1>", lambda x: verifier(tx_seq.get(), gsq_seq.get()))
-        receive_button.bind("<ButtonRelease-1>", lambda x: receiver(len(tx_seq.get()), rx_seq.get(),gsq_seq.get()))
+        generat_button.bind("<ButtonRelease-1>", lambda x: generator(msg_seq.get(), gsq_seq.get(),
+                                                                     tx_seq, output_terminal))
+        verifie_button.bind("<ButtonRelease-1>", lambda x: verifier(tx_seq.get(), gsq_seq.get(),
+                                                                    output_terminal))
+        receive_button.bind("<ButtonRelease-1>", lambda x: receiver(len(tx_seq.get()), rx_seq.get(),
+                                                                    gsq_seq.get(), output_terminal))
+        alterer_button.bind("<ButtonRelease-1>", lambda x: alter(gsq_seq.get(), tx_seq.get(),
+                                                                 ex_seq.get(), output_terminal))
 
         generat_button.grid(row=5, column=1, pady=pady_command_buttons, padx=padx_command_buttons)
         verifie_button.grid(row=5, column=3, pady=pady_command_buttons, padx=padx_command_buttons)
